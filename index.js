05935cdaaa887283e340a0cf6d6b10bccc441d9f
oppabgnamdz <oppabgnamdz@gmail.com>
2021-06-18 20:05:31 +0700

    demo get image

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var multer = require("multer");
 var upload = multer({ dest: "uploads/" });
 const path = require("path");
 var app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/public", express.static("public"));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
@@ -11,6 +15,9 @@ app.post("/profile", upload.single("avatar"), function (req, res, next) {
   // req.body will hold the text fields, if there were any
   console.log("post file");
   res.send("Done");
+});
+app.get("/ahihi", (req, res) => {
+  res.sendFile(path.join(__dirname + "/uploads/ccc.jpg"));
 });
 
 app.listen(8000);
