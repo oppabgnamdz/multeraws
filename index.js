bea3093a8053537c8ad77ce1506e43ddbc0cd3d0
oppabgnamdz <oppabgnamdz@gmail.com>
2021-06-18 19:31:27 +0700

    demo to aws

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var multer = require("multer");
+var upload = multer({ dest: "uploads/" });
+const path = require("path");
+var app = express();
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+app.post("/profile", upload.single("avatar"), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  console.log("post file");
+  res.send("Done");
+});
+
+app.listen(8000);
